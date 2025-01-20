bfe3aa49d5e282f9e0c06b15690e0abbff2ff6d0
shaharbaror1 <shaharbaror1@gmail.com>
2025-01-20 10:31:26 +0200

    to stash

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CSS/App.css";
 
 import Temp from "./Components/Temp.tsx";
 
 function App() {
+  const [userInput, setUserInput] = useState<string>("");
   return (
     <div className='App'>
       <h1>Hello World!</h1>
-
+      <input
+        type='text'
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <button>Send to server</button>
       <Temp username='hello' password='worlds?' />
       <h1>hi shahar</h1>
     </div>
